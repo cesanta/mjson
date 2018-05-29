85e8022947b08e51acc93d9a464ad7ba3786c78a
Sergey Lyubka <sergey.lyubka@cesanta.com>
2018-05-29 08:21:19 +0100

    More tests

--- mjson.h
+++ mjson.h
@@ -1,21 +1,30 @@
 // Copyright (c) 2018 Sergey Lyubka
 // All rights reserved
 
-typedef void (*mjson_cb_t)(int ev, char *s, int len, void *ud);
+enum {
+  MJSON_ERROR_INVALID_INPUT = -1,
+  MJSON_ERROR_TOO_DEEP = -2,
+  MJSON_ERROR_INCOMPLETE = -3,
+};
 
-enum { MJSON_ERROR_INVALID_INPUT = -1, MJSON_ERROR_INCOMPLETE_INPUT = 0 };
+enum mjson_tok {
+  MSJON_TOK_INVALID,
+  MJSON_TOK_STR,
+  MJSON_TOK_NUM,
+  MJSON_TOK_TRUE,
+  MJSON_TOK_FALSE,
+  MJSON_TOK_NULL,
+};
 
-static int mjson_is(int c, const char *choices) {
-  int i;
-  for (i = 0; choices[i] != '\0'; i++)
-    if (choices[i] == c) return 1;
-  return 0;
-}
+typedef void (*mjson_cb_t)(int ev, const char *s, int len, void *ud);
+
+#ifndef MJSON_MAX_DEPTH
+#define MJSON_MAX_DEPTH 20
+#endif
 
 static int mjson_pass_string(const char *s, int len) {
-  int i;
-  for (i = 0; i < len; i++) {
-    if (s[i] == '\\' && i + 1 < len && mjson_is(s[i + 1], "\b\f\n\r\t\\\"/")) {
+  for (int i = 0; i < len; i++) {
+    if (s[i] == '\\' && i + 1 < len && strchr("\b\f\n\r\t\\\"/", s[i + 1])) {
       i++;
     } else if (s[i] == '"') {
       return i;
@@ -24,47 +33,60 @@ static int mjson_pass_string(const char *s, int len) {
   return MJSON_ERROR_INVALID_INPUT;
 }
 
-static int mjson(char *s, int len, mjson_cb_t cb, void *ud) {
+static int mjson(const char *s, int len, mjson_cb_t cb, void *ud) {
   enum { S_VALUE, S_KEY, S_COLON, S_COMMA_OR_EOO } expecting = S_VALUE;
-  unsigned char nesting[20];
+  unsigned char nesting[MJSON_MAX_DEPTH];
   int i, depth = 0;
-// #define MJSONCALL(ev) cb(ev, s, i, &s[start], i - start, ud)
-#define MJSONCALL(ev) cb(ev, s, i + 1, ud)
+#define MJSONCALL(ev) \
+  if (cb) cb(ev, &s[start], i - start + 1, ud)
 
   for (i = 0; i < len; i++) {
     int start = i;
     unsigned char c = ((unsigned char *) s)[i];
+    enum mjson_tok tok = MSJON_TOK_INVALID;
     if (c == ' ' || c == '\t' || c == '\n' || c == '\r') continue;
     switch (expecting) {
       case S_VALUE:
         if (c == '{') {
+          if (depth >= (int) sizeof(nesting)) return MJSON_ERROR_TOO_DEEP;
           nesting[depth++] = c;
           expecting = S_KEY;
           break;
         } else if (c == '[') {
+          if (depth >= (int) sizeof(nesting)) return MJSON_ERROR_TOO_DEEP;
           nesting[depth++] = c;
           break;
         } else if (c == 't' && i + 3 < len && memcmp(&s[i], "true", 4) == 0) {
           i += 3;
+          tok = MJSON_TOK_TRUE;
         } else if (c == 'n' && i + 3 < len && memcmp(&s[i], "null", 4) == 0) {
           i += 3;
+          tok = MJSON_TOK_NULL;
         } else if (c == 'f' && i + 4 < len && memcmp(&s[i], "false", 5) == 0) {
           i += 4;
+          tok = MJSON_TOK_FALSE;
         } else if (c == '-' || ((c >= '0' && c <= '9'))) {
           if (c == '-') i++;
-          while (i < len && s[i] >= '0' && s[i] <= '9') i++;
+          while (i < len && s[i + 1] >= '0' && s[i + 1] <= '9') i++;
+          tok = MJSON_TOK_NUM;
+        } else if (c == '"') {
+          int n = mjson_pass_string(&s[i + 1], len - i);
+          if (n < 0) return n;
+          i += n + 1;
+          tok = MJSON_TOK_STR;
         } else {
           return MJSON_ERROR_INVALID_INPUT;
         }
-        if (depth == 0) return i;
+        if (depth == 0) return i + 1;
         expecting = S_COMMA_OR_EOO;
         break;
 
       case S_KEY:
         if (c == '"') {
           int n = mjson_pass_string(&s[i + 1], len - i);
-          if (n <= 0) return n;
+          if (n < 0) return n;
           i += n + 1;
+          tok = MJSON_TOK_STR;
           expecting = S_COLON;
         } else {
           return MJSON_ERROR_INVALID_INPUT;
@@ -74,6 +96,7 @@ static int mjson(char *s, int len, mjson_cb_t cb, void *ud) {
       case S_COLON:
         if (c == ':') {
           expecting = S_VALUE;
+          tok = ':';
         } else {
           return MJSON_ERROR_INVALID_INPUT;
         }
@@ -83,37 +106,20 @@ static int mjson(char *s, int len, mjson_cb_t cb, void *ud) {
         if (depth <= 0) return MJSON_ERROR_INVALID_INPUT;
         if (c == ',') {
           expecting = (nesting[depth - 1] == '{') ? S_KEY : S_VALUE;
+          tok = ',';
         } else if (c == ']' || c == '}') {
-          // printf("AAAA %c %c\n", c, nesting[depth - 1] + 2);
           // In ascii table, the distance between `[` and `]` is 2.
           // Ditto for `{` and `}`. Hence +2 in the code below.
           if (c != nesting[depth - 1] + 2) return MJSON_ERROR_INVALID_INPUT;
           depth--;
-          if (depth == 0) return i;
-          expecting = (nesting[depth - 1] == '{') ? S_KEY : S_COMMA_OR_EOO;
+          if (depth == 0) return i + 1;
+        } else {
+          return MJSON_ERROR_INVALID_INPUT;
         }
         break;
     }
-    printf("%.*s\t%d %d\n", i - start + 1, &s[start], depth, expecting);
-    // MJSONCALL(expecting);
+    // printf("%.*s\t%d %d\n", i - start + 1, &s[start], depth, expecting);
+    MJSONCALL(tok);
   }
-  return MJSON_ERROR_INVALID_INPUT;
+  return MJSON_ERROR_INCOMPLETE;
 }
-
-#if 0
-enum mjson_tok {
-  MSJON_TOK_INVALID,
-  MJSON_TOK_STR,
-  MJSON_TOK_NUM,
-  MJSON_TOK_TRUE,
-  MJSON_TOK_FALSE,
-  MJSON_TOK_NULL,
-  MJSON_TOK_OBJ,
-  MJSON_TOK_ARR,
-  MJSON_TOK_COMMA,
-  MJSON_TOK_COLON,
-};
-
-enum mjson_tok mjson_find(const char *s, int len, const char **ptr, int *n) {
-}
-#endif
--- mjson_test.c
+++ mjson_test.c
@@ -4,19 +4,40 @@
 
 #include "mjson.h"
 
-static void cb(int ev, char *s, int len, void *ud) {
+static void cb(int ev, const char *s, int len, void *ud) {
   // const char *states[] = {"value", "literal", "string", "escape", "utf8"};
   // const char *actions[] = {"none", "start", "end", "start_s", "end_s"};
   // printf("%s\t%s\t[%.*s]\n", states[st], actions[ac], len, s);
-  const char *names[] = {"value", "key", "colon", "comma"};
-  printf("%d\t%s\t[%.*s]\n", ev, names[ev], len, s);
+  // const char *names[] = {"value", "key", "colon", "comma"};
+  printf("%d\t[%.*s]\n", ev, len, s);
   (void) ud;
 }
 
 static void test_cb(void) {
-  char s[] = "{\"a\": true, \"b\": [ null, 3 ]}";
-  int n = mjson(s, sizeof(s) - 1, cb, NULL);
-  printf("n = %d\n", n);
+  {
+    const char *s = "{\"a\": true, \"b\": [ null, 3 ]}";
+    assert(mjson(s, strlen(s), cb, NULL) == (int) strlen(s));
+  }
+  {
+    const char *s = "[ 1, 2 ,  null, true,false,\"foo\"  ]";
+    assert(mjson(s, strlen(s), cb, NULL) == (int) strlen(s));
+  }
+  {
+    const char *s = "123";
+    assert(mjson(s, strlen(s), cb, NULL) == (int) strlen(s));
+  }
+  {
+    const char *s = "\"foo\"";
+    assert(mjson(s, strlen(s), cb, NULL) == (int) strlen(s));
+  }
+  {
+    const char *s = "123 ";  // Trailing space
+    assert(mjson(s, strlen(s), cb, NULL) == (int) strlen(s) - 1);
+  }
+  {
+    const char *s = "[[[[[[[[[[[[[[[[[[[[[";
+    assert(mjson(s, strlen(s), cb, NULL) == MJSON_ERROR_TOO_DEEP);
+  }
 }
 
 int main() {
