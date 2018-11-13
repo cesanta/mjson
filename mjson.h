cea4d30fa5c9a52e74386a7cfb01ece5e5f1a63c
Sergey Lyubka <sergey.lyubka@cesanta.com>
2018-11-13 19:33:36 +0000

    Moving to Cesanta

--- mjson.h
+++ mjson.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Sergey Lyubka
+// Copyright (c) 2018 Cesanta Software Limited
 // All rights reserved
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
